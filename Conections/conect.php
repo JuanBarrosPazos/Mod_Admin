c22e647b70dc8345902d85ead9e2cd66dab11b6d
Juan Barrós Pazos <juanbarrospazos@hotmail.es>
2023-07-15 12:43:10 +0200

    Add files via upload

---
+++ Conections/conect.php
@@ -0,0 +1,10 @@
+<?php
+
+	global $db;
+
+    mysqli_report(MYSQLI_REPORT_OFF);
+    $db = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+    if (!$db){ /*die*/ echo ("Es imposible conectar con la bbdd ".$db_name."</br>".mysqli_connect_error());
+            }
+            
+?>
